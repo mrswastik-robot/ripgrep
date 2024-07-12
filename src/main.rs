3d994cd6b0be108bc308032bfb0600ff88a654c1
Swastik Patel <swastikpatel29@gmail.com>
2024-07-12 18:52:44 +0530

    slight refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
 
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    print!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In the file {}", config.file_path);
+
+    //Reading the file!
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Wasn't able to read the contents of the file.");
+
+    println!("\nWith text:\n{contents}");
+
+
+    struct Config {
+        query: String,
+        file_path: String,
+    }
+
+    fn parse_config(args: &Vec<String>) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
 
