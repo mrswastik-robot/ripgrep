5b6f3a3724753c06c81c6e11bbff381c890e07e9
Swastik Patel <swastikpatel29@gmail.com>
2024-07-12 23:24:10 +0530

    clean output Error msgs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In the file {}", config.file_path);
@@ -21,12 +25,23 @@ fn main() {
         query: String,
         file_path: String,
     }
+    
+    impl Config {
+        
+        fn build (args: &Vec<String>) -> Result<Config, &str> {
+            
+            if args.len() < 3
+            {
+                return Err("not enough parameters");
+            }
 
-    fn parse_config(args: &Vec<String>) -> Config {
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+            let query = args[1].clone();
+            let file_path = args[2].clone();
 
-        Config { query, file_path }
+            Ok(Config { query, file_path })
+        }
+
     }
+   
 }
 
