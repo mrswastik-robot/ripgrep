61788784bcd904eb6ba2def38b0ce1824b991b51
Swastik Patel <swastikpatel29@gmail.com>
2024-07-14 00:24:28 +0530

    errors getting printed on the terminal and output in the file

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ use ripgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::build(&args).unwrap_or_else(|_err| {
-        //println!("Problem parsing arguments: {err}");
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     //println!("Searching for {}", config.query);
     //println!("In the file {}", config.file_path);
 
-    if let Err(_e) = ripgrep::run(config) {
-        //println!("Application Error: {e}");
+    if let Err(e) = ripgrep::run(config) {
+        eprintln!("Application Error: {e}");
         process::exit(1);
     }
 
