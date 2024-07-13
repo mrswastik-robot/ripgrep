86637920f9ea76d2437b8134615a89ee9b90440c
Swastik Patel <swastikpatel29@gmail.com>
2024-07-13 23:35:12 +0530

    searching functionality

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,16 @@ use ripgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    let config = Config::build(&args).unwrap_or_else(|_err| {
+        //println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In the file {}", config.file_path);
+    //println!("Searching for {}", config.query);
+    //println!("In the file {}", config.file_path);
 
-    if let Err(e) = ripgrep::run(config) {
-        println!("Application Error: {e}");
+    if let Err(_e) = ripgrep::run(config) {
+        //println!("Application Error: {e}");
         process::exit(1);
     }
 
